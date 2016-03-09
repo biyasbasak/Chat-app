1320386a868ceeda339a5c7fa5cec2cee46502f3
Biyas Basak <basakbiyas001@gmail.com>
2016-03-10 03:23:23 +0530

    added bootstrap

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,11 @@ var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
 var socket = io();
 
-$('#room').text(room);
+$('.room').text(room);
 
 console.log(name + 'wants to join ' + room);
 
-socket.on('connect', function () {
+socket.on('connect', function() {
 	console.log('connected to socket.io server');
 	socket.emit('joinRoom', {
 		name: name,
@@ -15,26 +15,28 @@ socket.on('connect', function () {
 });
 
 //listens to the message event and then displays the text
-socket.on('message', function(message){
+socket.on('message', function(message) {
 	var momentTimestamp = moment.utc(message.timestamp);
-	var $message = $('.messages');
+	var $messages = $('.messages');
+	var $message = $('<li class = "list-group-item"></li>')
 	console.log('new message:');
 	console.log(message.text);
 	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
 	$message.append('<p>' + message.text + '</p>');
+	$messages.append($message);
 });
 
 // Handles submitting of new message
 var $form = $('#message-form');
 
 //listens for submit event 
-$form.on('submit', function(event){
-	event.preventDefault();  //prevent the page from refreshing
-	
+$form.on('submit', function(event) {
+	event.preventDefault(); //prevent the page from refreshing
+
 	var $message = $form.find('input[name=message]');
-	
+
 	//creating a custom event message for the server to listen to
-	socket.emit('message',{
+	socket.emit('message', {
 		name: name, // name of the user
 		text: $message.val() // text typed
 	});
